b9f0f9f25ea0b9b7b6414f0339a710e0fd206952
Andrewowalla <andrewowalla@gmail.com>
2022-02-13 18:22:53 +0300

    authenticated routes and congiguring models

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -7,3 +8,7 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/product/comment',methods= ['GET','POST'])
+@login_required
+def new_comment():
